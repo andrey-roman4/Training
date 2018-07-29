cdf01c8830068c98191ab6f71b43de78f39ed4b1
andrey-roman4 <ozavir9022@gmail.com>
2018-07-29 22:16:00 +0300

    Rook progress

---
+++ Rook in chees/Rook.py
@@ -0,0 +1,8 @@
+a = int (input()) #Сможет ли ладья одним ходом попасть из первой кледки во вторую?
+b = int (input())
+a2 = int (input())
+b2 = int (input())
+if a == a2 or b == b2:
+    print('YES')
+else:
+    print('NO')
