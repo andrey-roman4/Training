34e9c68724b955e614e62a4d86d6299c6db08ae6
andrey-roman4 <ozavir9022@gmail.com>
2018-08-03 12:55:34 +0300

    Last changes

--- Task2/task2.2.py
+++ Task2/task2.2.py
@@ -1,6 +1,7 @@
 sentence = "House deemed an inappropriate manner for an event in the Oval Office."
 k = 0
+x = str (input())
 for i in sentence:
-    if i == 'f':
+    if i == x:
         k = k + 1
         print (k)
