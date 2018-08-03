76c0fc3a1b16266e38fb098b30f080704192798c
andrey-roman4 <ozavir9022@gmail.com>
2018-08-03 12:14:14 +0300

    Finalization New

--- Task2/task2.2.py
+++ Task2/task2.2.py
@@ -2,5 +2,5 @@ sentence = "House deemed an inappropriate manner for an event in the Oval Office
 k = 0
 for i in sentence:
     if i == 'f':
-        k = k +1
-        print (i)
+        k = k + 1
+        print (k)
