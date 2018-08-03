db122ea8c6faa2778c996e19b5953774570bd864
andrey-roman4 <ozavir9022@gmail.com>
2018-08-03 12:11:36 +0300

    Finalization

--- Task2/task2.2.py
+++ Task2/task2.2.py
@@ -1,5 +1,6 @@
 sentence = "House deemed an inappropriate manner for an event in the Oval Office."
+k = 0
 for i in sentence:
     if i == 'f':
-        print(i)
-        #while
+        k = k +1
+        print (i)
