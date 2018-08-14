82b3d37d85481dab37f5b9c4f86d0c7ad9d8e1bb
andrey-roman4 <ozavir9022@gmail.com>
2018-08-14 17:02:34 +0300

    Link to Yandex help

--- YaRu/Ya2.py
+++ YaRu/Ya2.py
@@ -3,5 +3,5 @@ import requests
 res = requests.get("https://yandex.ru/search/?text=testing")
 page_source = res.text
 
-results = page_source.split("serp-adv__found")[1].split(">")[0].split("</div")[0]
+results = page_source.split('class="link')[1].split('href="')[1].split('"')[0]
 print(results)
