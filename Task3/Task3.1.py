cacac257236059072f71a639192ba9af47bd9667
andrey-roman4 <ozavir9022@gmail.com>
2018-08-21 18:24:41 +0300

    deleted unnecessary lines

--- Task3/Task3.1.py
+++ Task3/Task3.1.py
@@ -3,15 +3,12 @@ import math
 z = input("Enter the word: " )
 #z = ("Repaper")
 z = z.lower()
-print(z)
 y = len(z)/2
 k = int (math.ceil(y))
 l = int (math.floor(y))
 a = z[:k]
 b = z[l:]
 h = b[::-1]
-print(a)
-print(h)
 if a==h:
     print ("Palyndrom")
 else:
